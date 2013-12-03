27b9d2b52f334aca003a0a6c0fb900373e8c2047
Juan G. Hurtado <juan.g.hurtado@gmail.com>
2013-12-03 18:43:15 +0100

    Add case for already installed dotfiles on install.sh script

--- install.sh
+++ install.sh
@@ -4,4 +4,6 @@ if [ ! -d "$HOME/.dotfiles" ]; then
 	git clone --recursive https://github.com/juanghurtado/dotfiles.git "$HOME/.dotfiles"
 	cd "$HOME/.dotfiles"
 	rake install
+else
+	echo "Dotfiles already installed. Please: cd ~/.dotfiles && git pull origin master && git submodule update && git submodule foreach git pull origin master"
 fi
