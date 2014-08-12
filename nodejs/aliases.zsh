c565e6cab826ec9bba4a374632be1e11968f82e8
Juan G. Hurtado <juan.g.hurtado@gmail.com>
2014-08-12 08:36:33 +0200

    Remove NodeJS aliases

--- nodejs/aliases.zsh
+++
@@ -1,9 +0,0 @@
-# NPM list without dependencies
-function npmls() {
-  npm ls --depth=0 "$@" 2>/dev/null
-}
-
-# NPM list (global) without dependencies
-function npmgls() {
-  npm -g ls --depth=0 "$@" 2>/dev/null
-}
