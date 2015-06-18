27367ba1f43c8ea83ab39060e83a7ce15b8ab674
Juan G. Hurtado <juan.g.hurtado@gmail.com>
2015-06-18 11:56:20 +0200

    Reconfigure todo-txt

--- todo-txt/aliases.zsh
+++ todo-txt/aliases.zsh
@@ -1,4 +1,4 @@
 export TODOTXT_SORT_COMMAND='env LC_COLLATE=C sort -k 2,2 -k 1,1n'
 export TODOTXT_DEFAULT_ACTION=ls
 
-alias t='todo.sh -p -d $HOME/.todo.cfg'
+alias t='todo.sh -a -t -d $HOME/.todo.cfg'
