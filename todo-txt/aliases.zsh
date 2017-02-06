b97beabb68b92c4fac02b1a6045771897c863945
Juan G. Hurtado <juan.g.hurtado@gmail.com>
2017-02-06 10:53:02 +0100

    Update todo-txt config

--- todo-txt/aliases.zsh
+++ todo-txt/aliases.zsh
@@ -1,6 +1,7 @@
 export TODOTXT_SORT_COMMAND='env LC_COLLATE=C sort -k 2,2 -k 1,1n'
 export TODOTXT_DEFAULT_ACTION=ls
 
-alias t='todo.sh -a -t -d $HOME/.todo.cfg'
+alias t='todo.sh -A -T -d $HOME/.todo.cfg'
 alias to='t ls @office'
 alias th='t ls @home'
+alias tp='t ls @personal'
