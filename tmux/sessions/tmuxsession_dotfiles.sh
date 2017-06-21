8cc4c9e3db4cba0d6806a0bbd6458a068abc8a8c
Juan G. Hurtado <juan.g.hurtado@gmail.com>
2017-06-21 09:07:19 +0200

    Don't close panes when processes stops

--- tmux/sessions/tmuxsession_dotfiles.sh
+++ tmux/sessions/tmuxsession_dotfiles.sh
@@ -4,7 +4,7 @@ SESSION_NAME="dotfiles"
 tmux has-session -t $SESSION_NAME
 if [ $? != 0 ]
 then
-  tmux new-session -s $SESSION_NAME -d -n editor -c $DOTFILES "vim ."
+  tmux new-session -s $SESSION_NAME -d -n editor -c $DOTFILES "vim .; zsh -l"
   tmux new-window -n git -t $SESSION_NAME: -c $DOTFILES
   tmux new-window -n misc -t $SESSION_NAME:
   tmux select-window -t $SESSION_NAME:1
